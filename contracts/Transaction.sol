9c4a64f5fda7b24a2a6400ca73b2d6932b2cd9df
satyaP <sats.pasupuleti@gmail.com>
2018-08-14 12:37:40 -0400

    added disputes to transactions

--- contracts/Transaction.sol
+++ contracts/Transaction.sol
@@ -8,14 +8,17 @@ contract Transaction{
     address public paidPerson;
     uint public time;
     ENFToken ENF;
+    bool inDispute=false;
+    address owner;
 /**
   * @dev Deploy contract after ENFToken is deployed, get ENFToken address
-  * @param _enf ENFToken address
+  * @param _enf ENFToken address (used in migrations)
   */
     constructor(address _enf) public
     {
         ENFadd = _enf;
         ENF = ENFToken(ENFadd);
+        owner=msg.sender;
 
         
     }
@@ -35,6 +38,7 @@ contract Transaction{
   */
     function transferBy() public payable{
         require(now >= time); //make sure funds are not paid before the time has elapsed
+        require(indispute=false); //prevents web3 from calling when the transaction is in dispute
         ENF.transfer(paidPerson,amount); //after time has elapsed web3.js will call this function 
     }
     function getAmount() public view returns(uint) {
@@ -46,6 +50,18 @@ contract Transaction{
     function getTime() public view returns(uint){
         return time;
     }
+    function setDispute() public{
+        require(msg.sender==owner); //only owner can call this function
+        indispute = true;
+
+    }
+    function resolve(address[] disputers,bool DisputerIsOwner) public{
+        //called by web3 after voters dispute the transaction
+        //give 10% of all the tokens in the contract to disputers
+        //give the rest of the tokens in the contract to the winner of dispute
+
+    }
+
     function () external payable {
     }
     
--- truffle.js
+++ truffle.js
@@ -16,15 +16,20 @@ module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 7545,
-      gas: 6500000,
-      network_id: "5777"
-    }
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+     ropsten:  {
+     network_id: 3,
+     host: "localhost",
+     port:  8545,
+     gas:   2900000,
+     from: '0xbD13E084990DaCe593C815c0b79EEEd2e17B632a'
+
+}
   },
-  solc: {
-     optimizer: {
-       enabled: true,
-       runs: 200
-     }
-  }
+   rpc: {
+ host: 'localhost',
+ post:8080
+   }
 };
