d65a8fff6a870f5fba69f4a45c75a13d100c6edf
satyaP <sats.pasupuleti@gmail.com>
2018-08-20 12:35:36 -0400

    updated tests

--- test/TestTransaction.js
+++ test/TestTransaction.js
@@ -1,20 +1,23 @@
 var ENFToken = artifacts.require("ENFToken");
 var Transaction = artifacts.require("Transaction");
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-
-
+//change to regular
 contract('Transaction', async(accounts)=> {
     it("Should start exchange properly", function() {
         var Account, trans;
         return ENFToken.deployed().then(function(instance) {
           return instance.balanceOf.call(accounts[0]); //500 million ENF in ENF contract
           Account=accounts[0];
-        }).then(function(balance){
+        }).then(async function(balance){
           return Transaction.deployed().then(function(instance){
-              instance.startExchange.call(10,accounts[1],20,{from:accounts[0]});
+              instance.startExchange.call(10,accounts[1],1,{from:accounts[0]});
              trans=instance;
 
-          }).then(function(done){
+          }).then(async function(done){
+            await delay(50000);
+             await trans.transferBy();
+             
              
              // this.timeout(30000); //timeout function for 10 seconds 
              //call transferBy
